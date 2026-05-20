49d58312145868ac42606b98224fbc318f2e93af
rezadehhaghi <rezadehhaghi.fi@gmail.com>
2026-05-20 15:43:52 +0300

    Create secret.h

---
+++ ESP32C3/secret.h
@@ -0,0 +1,14 @@
+
+  #pragma once
+
+  const char* WIFI_SSID = "YOUR_WIFI_NAME";
+  const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+  const char* THINGSPEAK_API_KEY = "YOUR_THINGSPEAK_API_KEY";
+
+  const char* INFLUX_URL =
+    "https://YOUR_REGION.aws.cloud2.influxdata.com/api/v2/write?org=YOUR_ORG&bucket=YOUR_BUCKET&precision=s";
+
+  const char* INFLUX_TOKEN = "YOUR_INFLUX_TOKEN";
+
+
